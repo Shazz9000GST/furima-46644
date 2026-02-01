d8ba86e3d5f9ad9ea43c07e3b81d2d6952d61614
Shazz9000GST <155403421+Shazz9000GST@users.noreply.github.com>
2026-02-01 17:51:51 +0900

    ユーザー登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  # deviseコントローラーで下記メソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   private
 
@@ -8,5 +11,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
 end
