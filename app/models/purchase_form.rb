b755414cd29328483e65633bbeb716270820ffef
Shazz9000GST <155403421+Shazz9000GST@users.noreply.github.com>
2026-03-08 22:05:11 +0900

    エラーハンドリング時のメッセージ表示

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -15,13 +15,19 @@ class PurchaseForm
   end
 
   validates :postal_code,
-            format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-
-  validates :phone_number,
-            format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
+            format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
 
   validates :prefecture_id,
             numericality: { other_than: 0, message: "can't be blank" }
+
+  validates :phone_number,
+            length: { minimum: 10, message: 'is too short' }
+
+  validates :phone_number,
+            length: { maximum: 11, message: 'is too long' }
+
+  validates :phone_number,
+            format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number' }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
